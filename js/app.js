77a4b3cdfd94da15c72ea905dac64c3d2145b791
Bogs Paulino <bogs.paulino@gmail.com>
2021-02-07 17:51:23 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+// User interaction animations
+// Mobile menu toggle
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click', () => {
+
+        // toggle nav
+        nav.classList.toggle('nav-active');
+
+        // // animate links
+        navLinks.forEach((link, index) => {
+
+            if (link.style.animation) {
+                link.style.animation = ``;
+            } else {
+                link.style.animation = `navLinkFadeIn 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        })
+
+        // burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+// Nav-links delay
+const navLinksDelay = () => {
+    const navLinks = document.querySelectorAll('.nav-links li');
+    let counter = 0;
+
+    for (var i = navLinks.length - 1; i >= 0; i--) {
+        counter++;
+        navLinks[i].classList.toggle("from-right");
+        navLinks[i].classList.toggle("ease");
+        navLinks[i].classList.toggle("forwards");
+        navLinks[i].classList.toggle("delay-" + counter);
+        navLinks[i].classList.toggle("duration-2");
+    }
+}
+
+const app = () => {
+    navLinksDelay();
+    navSlide();
+}
+
+app();
